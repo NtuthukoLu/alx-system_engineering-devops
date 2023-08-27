ede7bc651d7dcfdcf8e7d2e9844328fd48d7f9b5
Ntuthuko <ntuthukolu@gmail.com>
2023-08-28 00:51:05 +0200

    config with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# using puppet to make changes to the default ssh config file
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
+}
+`
