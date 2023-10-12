ca36dd66d3728c4853c9076d82f736ca52a81f7f
Ntuthuko <ntuthukolu@gmail.com>
2023-10-12 10:37:03 +0200

    $sMsg

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script for parsing web data from an api
+"""
+import requests
+
+
+def top_ten(subreddit):
+    base_url = f'https://www.reddit.com/r/{subreddit}/top.json?limit=10'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) '
+                      'Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+
+    try:
+        response = requests.get(base_url, headers=headers)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        data = response.json()
+
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+
+            for i, post in enumerate(posts[:10]):
+                print(post['data']['title'])
+
+        else:
+            print('No data found')
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+if __name__ == '__main__':
+    subreddit = 'python'  # Change this to the subreddit you want to retrieve
+    top_ten(subreddit)
+
